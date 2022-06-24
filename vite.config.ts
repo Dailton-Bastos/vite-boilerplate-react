1128d0740d206114b187f4cda01e580285c506d2
Dailton Bastos <dailtonbastos@gmail.com>
2022-06-23 22:26:10 -0300

    build(vite.config): remove outDir config

--- vite.config.ts
+++ vite.config.ts
@@ -9,9 +9,6 @@ import viteSvgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), viteSvgr()],
-  build: {
-    outDir: '../dist',
-  },
   test: {
     globals: true,
     environment: 'jsdom',
